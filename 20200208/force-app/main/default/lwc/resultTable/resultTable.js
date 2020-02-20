bd4318a951247d01b1034367a6948541b50dd103
ronny25089 <ronny25089@gmail.com>
2020-02-20 23:14:11 +0900

    🌲动态检索数据

---
+++ 20200208/force-app/main/default/lwc/resultTable/resultTable.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import { LightningElement, track, api } from 'lwc';
+
+const columns = [
+    { label: '商談名', fieldName: 'Name' },
+    { label: 'お客様名', fieldName: 'CustomerName__c' },
+    { label: '金額', fieldName: 'Amount', type: 'currency' },
+    { label: '営業店コード', fieldName: 'ShopCode__c' },
+    { label: '代理店コード', fieldName: 'AgentCode__c' },
+    { label: '代理店枝番', fieldName: 'AgentSubCode__c' },
+    { label: '契約者（団体）コード', fieldName: 'ContractorCodeGroup__c' },
+    { label: '担当（案件）', fieldName: 'ChargedPerson__c' },
+    { label: 'サブ担当１', fieldName: 'SubChargedPerson1__r.Name' },
+    { label: 'サブ担当２', fieldName: 'SubChargedPerson2__r.Name' },
+    { label: '担当ラベル', fieldName: 'ChargedPersonLabel__c' },
+    { label: '担当の所属', fieldName: 'ChargedBelong__c' }
+];
+
+
+export default class ResultTable extends LightningElement {
+    @track columns = columns;
+    @api list;
+}
--- 20200208/force-app/main/default/lwc/tab02/tab02.js
+++ 20200208/force-app/main/default/lwc/tab02/tab02.js
@@ -1,74 +1,86 @@
+/* eslint-disable no-console */
 import { LightningElement, track } from 'lwc';
+
+import findOpportunity from '@salesforce/apex/OpportunityApi.findOpportunity';
 
 export default class App extends LightningElement {
 
+    // 営業店コード: ShopCode
+    // 代理店コード: AgentCode
+    // 代理店枝番: AgentSubCode
+    // 契約者（団体）コード: ContractorCod
+    // 担当（案件）: ChargedPerson
+    // サブ担当１: SubChargedPer
+    // サブ担当２: SubChargedPer
+    // 担当ラベル: ChargedPerson
+    // 担当の所属: ChargedBelong
     // 定义tab切换的标签显示规则
     rules = {
         selectAll: {
-            shopCode: true,
-            agentCode: true,
-            agentSubCode: true,
-            contractorCodeGroup: true,
-            chargedPerson: true,
-            subChargedPerson1: true,
-            subChargedPerson2: true,
-            chargedPersonLabel: true,
-            chargedBelong: true
+            ShopCode: true,
+            AgentCode: true,
+            AgentSubCode: true,
+            ContractorCodeGroup: true,
+            ChargedPerson: true,
+            SubChargedPerson1: true,
+            SubChargedPerson2: true,
+            ChargedPersonLabel: true,
+            ChargedBelong: true
         },
         renewal: {
-            shopCode: false,
-            agentCode: false,
-            agentSubCode: false,
-            contractorCodeGroup: true,
-            chargedPerson: true,
-            subChargedPerson1: true,
-            subChargedPerson2: true,
-            chargedPersonLabel: true,
-            chargedBelong: true
+            ShopCode: false,
+            AgentCode: false,
+            AgentSubCode: false,
+            ContractorCodeGroup: true,
+            ChargedPerson: true,
+            SubChargedPerson1: true,
+            SubChargedPerson2: true,
+            ChargedPersonLabel: true,
+            ChargedBelong: true
         },
         insNew: {
-            shopCode: true,
-            agentCode: true,
-            agentSubCode: true,
-            contractorCodeGroup: false,
-            chargedPerson: false,
-            subChargedPerson1: false,
-            subChargedPerson2: true,
-            chargedPersonLabel: true,
-            chargedBelong: true
+            ShopCode: true,
+            AgentCode: true,
+            AgentSubCode: true,
+            ContractorCodeGroup: false,
+            ChargedPerson: false,
+            SubChargedPerson1: false,
+            SubChargedPerson2: true,
+            ChargedPersonLabel: true,
+            ChargedBelong: true
         },
         lifeIns: {
-            shopCode: true,
-            agentCode: true,
-            agentSubCode: true,
-            contractorCodeGroup: true,
-            chargedPerson: true,
-            subChargedPerson1: true,
-            subChargedPerson2: false,
-            chargedPersonLabel: false,
-            chargedBelong: false
+            ShopCode: true,
+            AgentCode: true,
+            AgentSubCode: true,
+            ContractorCodeGroup: true,
+            ChargedPerson: true,
+            SubChargedPerson1: true,
+            SubChargedPerson2: false,
+            ChargedPersonLabel: false,
+            ChargedBelong: false
         },
         superIns: {
-            shopCode: true,
-            agentCode: false,
-            agentSubCode: true,
-            contractorCodeGroup: false,
-            chargedPerson: true,
-            subChargedPerson1: true,
-            subChargedPerson2: false,
-            chargedPersonLabel: true,
-            chargedBelong: true
+            ShopCode: true,
+            AgentCode: false,
+            AgentSubCode: true,
+            ContractorCodeGroup: false,
+            ChargedPerson: true,
+            SubChargedPerson1: true,
+            SubChargedPerson2: false,
+            ChargedPersonLabel: true,
+            ChargedBelong: true
         },
         common: {
-            shopCode: true,
-            agentCode: false,
-            agentSubCode: true,
-            contractorCodeGroup: true,
-            chargedPerson: false,
-            subChargedPerson1: true,
-            subChargedPerson2: true,
-            chargedPersonLabel: true,
-            chargedBelong: true
+            ShopCode: true,
+            AgentCode: false,
+            AgentSubCode: true,
+            ContractorCodeGroup: true,
+            ChargedPerson: false,
+            SubChargedPerson1: true,
+            SubChargedPerson2: true,
+            ChargedPersonLabel: true,
+            ChargedBelong: true
         }
     };
     /**
@@ -83,7 +95,12 @@ export default class App extends LightningElement {
     @track
     showFeatures = this.rules.selectAll;
 
+    // 控制搜索结果显示
+    @track
+    dataResults;
+
     /**
+     * 切换标签
      * @param event 触发事件对象
      * @param event.target 触发的标签lightning-tab
      */
@@ -93,4 +110,43 @@ export default class App extends LightningElement {
         this.showFeatures = this.rules[value];
     }
 
+    /**
+     * 获取当前页面有效的所有输入框:<lightning-input></lightning-input>
+     * 组合成Map型数据，调用Apex的findOpportunity(Map<String,String> keyMap)
+     * 将Apex方法中执行的SOQL结果赋值给dataResults
+     * 通过子页面中声明的@api公共变量，传递给子页面resutlTable.html显示结果数据。
+     */
+    searchRecord() {
+        // 检索键Map
+        let keyMap = {};
+        // 获取当前页面有效的所有输入框
+        let inputList = this.template.querySelectorAll('lightning-input');
+
+        for (let i = 0; i < inputList.length; i++) {
+            // 只对有值项目进行处理
+            if (inputList[i].value !== '') {
+                // 将有效字段，按键值对存进keyMap
+                keyMap[inputList[i].name] = inputList[i].value;
+            }
+        }
+        // 调用Apex的findOpportunity(Map<String,String> keyMap)
+        // 获取结果集
+
+        findOpportunity({ keyMap }).then(result => {
+                console.log("dataResults", result);
+                this.dataResults = result;
+            })
+            .catch(error => {
+                console.log("error", error);
+            });
+
+        // ApexMethod({params...}).then(result => {
+        //     doSomething...
+        // }).catch(error => {
+        //     doSomething...
+        // });
+        //
+        // .then()为promise 回调函数，用于处理结果
+        // result => {}为 匿名函数ES6特性，也可以写作funciton(){}
+    }
 }
